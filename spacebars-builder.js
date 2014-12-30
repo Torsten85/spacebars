8cc12a378a4953be23b05902dabf277eda0025f7
Torsten Blindert <torsten@Torstens-MacBook-Pro.local>
2014-12-31 00:44:30 +0100

    Initial commit

---
+++ spacebars-builder.js
@@ -0,0 +1,38 @@
+define(function () {
+
+  var config = requirejs.s.contexts._.config;
+  config.context = 'new';
+
+  var newRequire = requirejs.config(config);
+
+  var Compiler = newRequire('blaze/spacebars-compiler');
+
+  var extension = 'hbs';
+
+  var buildMap = {};
+  var fs = nodeRequire('fs');
+
+  function fetchText (path) {
+    var body = fs.readFileSync(path, 'utf8') || '';
+    return body.replace(/^uFEFF/, '');
+  }
+
+  var api = {};
+
+  api.write = function (pluginName, name, write) {
+    if (name in buildMap) {
+      write("define('" + pluginName + "!" + name + "', ['blaze', 'blaze/htmljs', 'blaze/spacebars'], function (Blaze, HTML, Spacebars) {\n" +
+      "return new Blaze.Template(" + buildMap[name] + ");\n" +
+      "});\n");
+    }
+  };
+
+  api.load = function (name, parentRequire, load, config) {
+    var path = parentRequire.toUrl(name + '.' + extension);
+    var content = fetchText(path);
+    buildMap[name] = Compiler.compile(content, {isTemplate: true});
+    load();
+  };
+
+  return api;
+});
---
+++ spacebars.js
@@ -0,0 +1,33 @@
+define(function () {
+
+  var extension = 'hbs';
+
+  function fetchText (path, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        callback(xhr.responseText);
+      }
+    };
+
+    xhr.send(null);
+  }
+
+  var api = {};
+
+  api.pluginBuilder = './spacebars-builder';
+
+  api.load = function (name, parentRequire, load, config) {
+    parentRequire(['blaze', 'blaze/htmljs', 'blaze/spacebars', 'blaze/spacebars-compiler'], function (Blaze, HTML, Spacebars, Compiler) {
+      var path = parentRequire.toUrl(name + '.' + extension);
+
+      fetchText(path, function (content) {
+        var renderFuncCode = Compiler.compile(content, {isTemplate: true});
+        load(new Blaze.Template(eval(renderFuncCode)));
+      });
+    });
+  };
+
+  return api;
+});
